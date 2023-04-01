10d0603d855fb2e8bed87136945d76fcc309dbd2
Prasanna Hegde <prasann.h89@gmail.com>
2023-04-01 21:24:00 +0530

    added more api

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
-
+from typing import Optional
+from schemas import BlogRequest
 app = FastAPI()
 
 
@@ -10,8 +11,37 @@ def index():
             "data": None}
 
 
-@app.get("/about")
-def about():
+@app.get("/blogs")
+def get_blogs_all(limit=10, sort: Optional[str] = None):
     return {"status": 200,
             "detail": "Success",
-            "data": "This is about section"}
+            "data": [f"{limit}"]}
+
+
+@app.get("/blogs/unpublished", description="Get All Unpublished blogs")
+def get_blogs_unpublished():
+    return {"status": 200,
+            "detail": "Success",
+            "data": ["List of unpublished blogs"]}
+
+
+@app.get("/blogs/{id}")
+def get_blogs_id(id: int):
+    return {"status": 200,
+            "detail": "Success",
+            "data": [{"id": id}]}
+
+
+@app.get("/blogs/{id}/comments")
+def get_blogs_id_comments(id: int):
+    return {"status": 200,
+            "detail": "Success",
+            "data": [{"id": id,
+                     "comments": ["1", "2"]}]}
+
+
+@app.post("/blogs")
+def create_blog(request: BlogRequest):
+    return {"status": 200,
+            "detail": "Success",
+            "data": {"id": request}}
