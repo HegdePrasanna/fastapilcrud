a7aec9ec8801a20e6742dc51ac03bc47c8b68486
Prasanna Hegde <prasann.h89@gmail.com>
2023-04-09 22:09:08 +0530

    added new schema for nested returns

--- schemas.py
+++ schemas.py
@@ -1,17 +1,12 @@
 from pydantic import BaseModel
-from typing import Optional
+from typing import Optional, List
 
 
 class BlogRequest(BaseModel):
     title: str
     body: str
+    user_id: int
     # published: Optional[bool]
-
-
-class BlogReturn(BaseModel):
-    id: int
-    title: str
-    body: str
 
     class Config:
         orm_mode = True
@@ -21,3 +16,41 @@ class UserRequest(BaseModel):
     name: str
     email: str
     password: str
+
+
+class UserReturn(BaseModel):
+    id: int
+    name: str
+    email: str
+    blogs: List[BlogRequest] = None
+
+    class Config:
+        orm_mode = True
+
+
+class UserReturn1(BaseModel):
+    id: int
+    name: str
+    email: str
+
+    class Config:
+        orm_mode = True
+
+
+class UserReturn2(BaseModel):
+    status: int = 200
+    detail: str = "Success"
+    data: List[UserReturn]
+
+    class Config:
+        orm_mode = True
+
+
+class BlogReturn(BaseModel):
+    id: int
+    title: str
+    body: str
+    created_by: UserReturn1 = None
+
+    class Config:
+        orm_mode = True
